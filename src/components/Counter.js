5beedcc926e1b2e3dce3642ec06d9111f9e2ea37
CCCLARITY <1227039581@qq.com>
2019-10-29 11:32:42 +0800

    first init

---
+++ src/components/Counter.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+// import classnames from 'classnames';
+import styles from './Counter.css';
+require('./Counter.css');
+
+class Counter extends Component{
+
+    // constructor(props){
+    //     super(props)
+
+    // }
+
+    render(){
+        return <div className={styles.wrapper}>
+            <div>点击了0次</div>
+            <button>+</button>
+            <button>-</button>
+            <button>当是奇数的时候增加</button>
+            <button>异步增加2s</button>
+        </div>
+    }
+}
+
+export default Counter;
